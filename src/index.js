87dfbc10f138325d77ce01f6b3f1a99bef56e2e5
JsnSDL <jason.d@sdlglobe.com>
2024-09-05 16:50:40 +0530

    Added redux for signup and login

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './assets/css/style.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { store, persistor } from './store/store'; 
+import { PersistGate } from 'redux-persist/integration/react';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
 
