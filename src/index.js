15598e107f6aef7616d31a3eb681460a6f1cdf02
Rakesh swain <142288684+rakesh-sdlglobe@users.noreply.github.com>
2024-10-03 17:20:18 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-     <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+     <GoogleOAuthProvider clientId=493628196678-cjvhttpbp9c4ha4a35srklc9skvgq324.apps.googleusercontent.com>
       <App />
       </GoogleOAuthProvider>
     </Provider>
